ac97e522c74f99bc68fc2b6165e72e84085d5b88
Vallu <vallu9@hotmail.com>
2025-10-17 14:13:18 +0300

    start

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# supercell-prac
+# Supercell-Challenge-2024
+This repo shall contain everything that involves the supercell challenge for hive helsinki
---
+++ survive/src/Player.cpp
@@ -0,0 +1,72 @@
+#include "Player.h"
+#include "Weapon.h"
+#include "InputHandler.h"
+#include "Constants.h"
+#include <vector>
+#include "Game.h"
+
+Player::Player(Game* pGame) :
+    Rectangle(sf::Vector2f(PlayerWidth, PlayerHeight)),
+    m_pGame(pGame),
+    m_pWeapon(std::make_unique<Weapon>())
+{
+    setOrigin(sf::Vector2f(0.0f, 0.0f));
+}
+
+bool Player::initialise()
+{
+    m_sprite.setTexture(*m_pGame->getPlayerTexture());
+    m_sprite.setScale(3.5f, 3.5f);
+    setIsDead(false);
+    setPosition(ScreenWidth / 2, ScreenHeight / 2);
+    m_sprite.setPosition(getPosition());
+    return true;
+}
+
+void Player::move(InputData inputData, float deltaTime)
+{
+    float xSpeed = 0.0f;
+    float ySpeed = 0.0f;
+    
+    xSpeed -= inputData.m_movingLeft * PlayerSpeed;
+    xSpeed += inputData.m_movingRight * PlayerSpeed;
+    xSpeed *= deltaTime;
+
+    ySpeed -= inputData.m_movingUp * PlayerSpeed;
+    ySpeed += inputData.m_movingDown * PlayerSpeed;
+    ySpeed *= deltaTime;
+    
+    sf::Transformable::move(sf::Vector2f(xSpeed, ySpeed));
+    setPosition(std::clamp(getPosition().x, 0.0f, (float)ScreenWidth), getPosition().y);
+
+    if (inputData.m_movingLeft && !inputData.m_movingRight)
+        m_direction = LEFT;
+    else if (!inputData.m_movingLeft && inputData.m_movingRight)
+        m_direction = RIGHT;
+    else if (inputData.m_movingUp && !inputData.m_movingDown)
+        m_direction = UP;
+    else if (!inputData.m_movingUp && inputData.m_movingDown)
+        m_direction = DOWN;
+}
+
+void Player::attack()
+{
+    m_pWeapon->setActive(true);
+}
+
+void Player::update(float deltaTime)
+{
+    sf::Vector2f weaponSize = m_pWeapon->getSize();
+
+    m_sprite.setPosition(getPosition());
+    m_pWeapon->setPosition(sf::Vector2f(
+        getCenter().x - (m_direction == LEFT ? weaponSize.x : 0.0f),
+        getCenter().y - weaponSize.y / 2.0f));
+    m_pWeapon->update(deltaTime);
+}
+
+void Player::draw(sf::RenderTarget &target, sf::RenderStates states) const
+{
+    Rectangle::draw(target, states);
+    m_pWeapon->draw(target, states);
+}
